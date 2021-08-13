1bb0133465fa7432cb7c476a124423ea5e2d2006
Raphael Aubry <raphael.aubry@hotmail.fr>
2021-08-13 16:50:02 +0200

    Controllers and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'movies/home'
+  get 'movies', to: 'movies#index', as: :movies
+  get 'movies/:id', to: 'movies#show', as: :movie
   get 'lists/home'
   get 'lists', to: 'lists#index'
   get 'lists/new', to: 'lists#new'
   post 'lists', to: 'lists#create'
   get 'lists/:id', to: 'lists#show', as: :list
+  get 'bookmarks/home'
+  get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_list_bookmark
+  post 'lists/:list_id/bookmarks', to: 'bookmarks#create', as: :list_bookmarks
+  resources :bookmarks, only: [ :destroy ]
 end
